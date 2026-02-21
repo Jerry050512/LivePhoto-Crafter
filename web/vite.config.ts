dcb8caf89ba36796faa45d8909da9f2f96ea7046
gang <jerry050512@outlook.com>
2026-02-21 21:32:16 +0800

    fix: update base path with env VITE_BASE_URL

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/LivePhoto-Crafter/',
+  base: process.env.VITE_BASE_URL || '/',
   plugins: [vue()],
   resolve: {
     alias: {
